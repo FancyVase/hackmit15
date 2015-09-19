09e361018c07fe3cfc448fd4897dc75d66f9ca09
Hiram <mm.hiram@gmail.com>
2015-09-19 18:05:58 -0400

    Add internet permissions - test works now

---
+++ app/src/main/java/hiram/xyz/hackmit15/MainActivity.java
@@ -0,0 +1,57 @@
+package hiram.xyz.hackmit15;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.IOException;
+
+import io.indico.Indico;
+import io.indico.network.IndicoCallback;
+import io.indico.results.IndicoResult;
+import io.indico.utils.IndicoException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Indico.init(this, getString(R.string.indico_api_key), null);
+
+        try {
+            Indico.sentiment.predict("indico is so easy to use!", new IndicoCallback<IndicoResult>() {
+                @Override public void handle(IndicoResult result) throws IndicoException {
+                    Log.i("Indico Sentiment", "sentiment of: " + result.getSentiment());
+                }
+            });
+        } catch (IOException | IndicoException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
